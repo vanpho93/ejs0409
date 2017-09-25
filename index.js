26efc93be167e0007cee8232f3fa30c329dd1e97
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-25 18:59:14 +0700

    Person

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ app.use(express.static('./public'));
 
 app.get('/', (req, res) => res.render('home'));
 
+app.get('/params', (req, res) => {
+    res.render('params', { a: 100 });
+});
+
 app.get('/a/b', (req, res) => res.render('ab'));
 
 reload(app);
 app.listen(3000, () => console.log('Server start!'));
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const arrPerson = [
+    new Person('Ti', 10),
+    new Person('Teo', 15),
+    new Person('Tun', 20)
+];
