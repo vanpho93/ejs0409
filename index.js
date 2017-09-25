6302c08eda1a57544cbe073ad4f487f0fc1c04f5
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-25 17:48:11 +0700

    serve file

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => res.render('home'));
+
+app.listen(3000);
