8589baddc4aac8a1337a795d5854660bb98985f1
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-25 17:54:39 +0700

    static files

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(express.static('./public'));
+
 app.get('/', (req, res) => res.render('home'));
 
 app.listen(3000);
